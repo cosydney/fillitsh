530ee9ea6a12c062c9dc7f88ed78b3d63ba34aa8
cosydney <sycohen@e2r8p1.42.fr>
2016-11-09 16:54:53 +0100

    validations

--- check_input.c
+++ check_input.c
@@ -6,42 +6,54 @@
 /*   By: hdelaby <hdelaby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 09:57:35 by hdelaby           #+#    #+#             */
-/*   Updated: 2016/11/09 11:20:28 by hdelaby          ###   ########.fr       */
+/*   Updated: 2016/11/09 16:54:20 by sycohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "fillit.h"
 
-int		syntax_check(char *str_data)
+int		chars_check(char str)
 {
-	int		char_lines;
-	int		nb_rows;
-
-	char_lines = 0;
-	nb_rows = 0;
-	while (*str_data)
-	{
-		ft_putchar(*str_data);
-		if ((*str_data != '.' && *str_data != '#' && *str_data != '\n') || char_lines > 4)
-			return (1);
-		if (*str_data == '\n')
-		{
-			if (char_lines != 4)
-				return (1);
-			char_lines = -1;
-			nb_rows++;
-		}
-		char_lines++;
-		str_data++;
-	}
-	return (0);
+	if (str != '.' && str != '#' && str != '\n')
+		return (1);
+	else
+		return (0);
 }
-		
+
+int		check(char *str)
+{
+	int line;
+	int height;
+
+	height = 0;
+	line = 0;
+	while (*str)
+	{
+		if (chars_check(*str))
+			return (1);
+		if (*str == '\n')
+		{
+			if (line != 4)
+				return (1);
+			line = -1;
+			height++;
+		}
+		if (*str == '\n' && *(str + 1) == '\n')
+		{
+			if (height % 4 != 0)
+				return (1);
+			str++;
+		}
+		line++;
+		str++;
+	}
+	return ((height % 4 == 0) ? 0 : 1);
+}
 
 char	*check_input(char *str_data)
 {
-	if (syntax_check(str_data))
+	if (check(str_data))
 		return (NULL);
 	return (str_data);
 }
