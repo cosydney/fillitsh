f2b6393518f2403eb3c9f55dc634ddc85873f113
cosydney <sycohen@e2r8p1.42.fr>
2016-11-10 18:55:04 +0100

    test2 updated and compare tetri to actual pieces

--- compare_tetri.c
+++ compare_tetri.c
@@ -6,43 +6,40 @@
 /*   By: sycohen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 16:49:25 by sycohen           #+#    #+#             */
-/*   Updated: 2016/11/10 18:08:56 by sycohen          ###   ########.fr       */
+/*   Updated: 2016/11/10 18:54:43 by sycohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "fillit.h"
 
-#include <stdio.h>
-
 int		compare_tetris(char **tetris)
 {
 	int		i;
 	int		j;
 	int		count;
-	char	**tetri;
+	char	**t;
 
 	i = 0;
-	count = 0; 
-	tetri = NULL;
-	tetri = malloc(150);
-	tetri[0] = strdup("1#\n#\n#\n#\n####\n2#.\n#.\n##\n2..#\n###\n2##\n.#\n.#\n2###\n#..\n");
-	tetri[1] = strdup("3.#.\n###\n3.#\n##\n.#\n3###\n.#.\n3#.\n##\n#.\n");
-	tetri[2] = strdup("4.#\n.#\n##\n4.###\n..#\n4##\n#.\n#.\n4#..\n###\n");
-	tetri[3] = strdup("5##\n##\n6##.\n.##\n6.#\n##\n#.\n7.##\n##.\n7#.\n##\n.#\n");
+	count = 0;
+	if(!(t = ft_memalloc(sizeof(char*) * 5)))
+		return (1);
+	t[0] = strdup("1#\n#\n#\n#\n1####\n");
+	t[1] = strdup("2#.\n#.\n##\n2..#\n###\n2##\n.#\n.#\n2###\n#..\n");
+	t[2] = strdup("3.#.\n###\n3.#\n##\n.#\n3###\n.#.\n3#.\n##\n#.\n");
+	t[3] = strdup("4.#\n.#\n##\n4.###\n..#\n4##\n#.\n#.\n4#..\n###\n");
+	t[4] = strdup("5##\n##\n6##.\n.##\n6.#\n##\n#.\n7.##\n##.\n7#.\n##\n.#\n");
 	while(tetris[i])
 	{
 		j = 0;
-		while (j < 4)
+		while (j < 5)
 		{
-			if (ft_strstr(tetri[j], tetris[i]))
+			if (ft_strstr(t[j], tetris[i]))
 				count++;
 			j++;
 		}
 		i++;
 	}
-	printf("count %d\n", count);
-	printf("i %d\n", i);
-
-	return (0);
+	free (t);
+	return (count == i ? 0 : 1);
 }
